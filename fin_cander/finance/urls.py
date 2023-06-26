8204b673372c29b5f1e7a36758004e10215367b1
karolmatuszak7@gmail.com <56365605+Kar977@users.noreply.github.com>
2023-06-26 19:10:12 +0200

    modified finance/models

--- fin_cander/finance/urls.py
+++ fin_cander/finance/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from finance.views.income.crud import FinanceView
+#from fin_cander.views.income.crud import FinanceView, AddIncome
+from finance.views.income.crud import FinanceView, AddIncome, CreateIncomePlanExecute
 
 
 urlpatterns = [
-	path("fincander/", FinanceView, name="fincander")
+	path("fincander/", FinanceView, name="fincander"),
+	path("fincander/income", AddIncome, name="add_income"),
+	path("fincander/CRUD", CreateIncomePlanExecute.as_view(), name="crud")
 ]
